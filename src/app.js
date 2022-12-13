e49a5ad720ffc9ad1db2d27908bb575f1dd7f457
Harigoth <ofbrunobm@gmail.com>
2022-12-13 01:13:03 -0300

    fix: http to https

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import MovieCard from './moviecard';
 // 9ca92cdd
  
 
-const API_URL = 'http://www.omdbapi.com/?apikey=9ca92cdd&'
+const API_URL = 'https://www.omdbapi.com/?apikey=9ca92cdd&'
 
 const App = () => {
     const [movies, SetMovies] = useState([]);
